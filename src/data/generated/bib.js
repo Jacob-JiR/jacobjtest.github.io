900800cbb85c0d2067805f43dd28954e9d7700bd
Jacob-JiR <146676639+Jacob-JiR@users.noreply.github.com>
2026-04-03 20:15:14 +0800

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,31 @@
+﻿define({ entries : {
+    "Beck2016Visual": {
+        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
+        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
+        "doi": "10.1109/TVCG.2015.2467757",
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
+        "number": "01",
+        "publisher": "IEEE",
+        "series": "TVCG",
+        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+        "type": "article",
+        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
+        "volume": "22",
+        "year": "2016"
+    },
+    "Firat2022Interctive": {
+        "abstract": " With the advent of novel visualization techniques to convey complex information, data visualization literacy is growing in importance. Two facets of literacy are user understanding and the discovery of visual patterns with the help of graphical representations. The research literature on visualization literacy provides useful guidance and important opportunities for further studies in this field. This survey examines and classifies prior research on visualization literacy that analyzes how well users understand novel data representations. To our knowledge, this is the first comprehensive survey paper with a focus on interactive visualization literacy. We categorize existing relevant research into unique subject groups that facilitate and inform comparisons of related literature and provide an overview of the same. Additionally, the survey/classification also provides an overview of the various evaluation techniques used in this field of research due to their challenging nature. Our novel classification enables researchers to find both mature and unexplored directions that may lead to future work. This survey serves as a valuable resource for both beginners and experienced researchers interested in the topic of visualization literacy.",
+        "author": "Elif E Firat and Alark Joshi and Robert S Laramee",
+        "doi": "10.1177/14738716221081831",
+        "eprint": " https://doi.org/10.1177/14738716221081831 ,",
+        "journal": "Information Visualization",
+        "number": "3",
+        "pages": "285-310",
+        "title": "Interactive visualization literacy: The state-of-the-art",
+        "type": "article",
+        "url": " https://doi.org/10.1177/14738716221081831 ,",
+        "volume": "21",
+        "year": "2022"
+    }
+}});
